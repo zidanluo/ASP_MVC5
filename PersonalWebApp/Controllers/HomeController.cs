d94aa4d1ddf0ae5938da102b265cf2dc542507ab
luoyp <luoyp@ncs.com.sg>
2015-11-30 17:28:21 +0800

    Configured Spring.Net ,successful passing thru
    Signed-off-by: luoyp <luoyp@ncs.com.sg>

--- PersonalWebApp/Controllers/HomeController.cs
+++ PersonalWebApp/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using AopAlliance.Intercept;
+using log4net;
+using Spring.Aop;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,23 +13,65 @@ namespace PersonalWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private ILog log = LogManager.GetLogger(typeof(HomeController));
+
+        public ICommand Cmd { get; set; }
         public ActionResult Index()
         {
+            Spring.Aop.Support.AttributeMatchMethodPointcutAdvisor s;
+            Spring.Aop.Support.NameMatchMethodPointcutAdvisor n;
+            log.Info("index");
+
+
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message = Cmd.exec("abc");// "Your application description page.";
+            log.Info("about");
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-            
+            ViewBag.Message = Cmd.exec("123");//"Your contact page.";
+            log.Info("contact");
             return View();
+        }
+    }
+
+    public interface ICommand
+    {
+        string exec(string context);
+    }
+
+    public class ServiceCommand : ICommand
+    {
+        public virtual string exec(string context)
+        {
+            ILog log = LogManager.GetLogger(typeof(ServiceCommand));
+            log.Info(string.Format("Service implementation : [{0}]", context));
+            return context;
+        }
+    }
+
+    public class LogCallInterceptor : IMethodBeforeAdvice
+    {
+        public void Before(MethodInfo method, object[] args, object target)
+        {
+            ILog log = LogManager.GetLogger(typeof(LogCallInterceptor));
+            log.Info(method.Name);
+        }
+    }
+
+    public class LogAfter : Spring.Aop.IAfterReturningAdvice
+    {
+        
+        public void AfterReturning(object returnValue, MethodInfo method, object[] args, object target)
+        {
+            ILog log = LogManager.GetLogger(typeof(LogCallInterceptor));
+            log.Info("After" + method.Name);
         }
     }
 }
